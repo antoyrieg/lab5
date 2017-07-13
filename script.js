144a9ace807ab3dfa73823714dda65f03b3fee92
Antoyrie Green <antoyrieg@gmail.com>
2017-07-13 15:33:06 -0400

    Lab 5

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log ("It's working!!");
+
+var tF= 70;
+
+console.log("The temperature in Celcius is " + (tF - 32) * (5/9) + 
+" degrees if in Farenheit it is " + tF + " degrees.")
+
+var tC = 30
+console.log ("The temperature in Farenheit is " + (tC*(9/5) + 32) +
+ " degrees if in Celcius it is " + tC + " degrees.")
